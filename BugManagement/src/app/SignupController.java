7b9f7cdafff08bc51b7ac841011c8c887659b9c6
myanhnguyen <114592790+myanhnguyen@users.noreply.github.com>
2022-11-27 00:29:59 +0700

    Update SignupController.java

--- BugManagement/src/app/SignupController.java
+++ BugManagement/src/app/SignupController.java
@@ -1,23 +1,19 @@
 package app;
 
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
-public class SignupController implements Initializable {
+public class SignupController {
     @FXML
     private TextField name;
     @FXML
@@ -29,123 +25,107 @@ public class SignupController implements Initializable {
     @FXML
     private PasswordField cfpass;
     @FXML
-    private Button signup;
-    @FXML
-    private Label namelabel;
-    @FXML
     private Label emaillabel;
     @FXML
-    private Label usernamelabel;
-    
+    private Label usernamelabel;   
+    @FXML
+    private Label cfpasslabel;
+    @FXML
+    private ToggleGroup chucvu;
     @FXML
     private Label passlabel;
     @FXML
-    private Label cfpasslabel;
-    
-    @FXML
-    private ToggleGroup chucvu;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
+    private Label namelabel;  
 
-    //Action when nextButton is pressed.
     @FXML
     public void singupOnAction() throws SQLException {
-        if (pass.getText().equals(cfpass.getText())) {
-            cfpasslabel.setText("");
-            signup();
-        } else {
-            cfpasslabel.setText("Password does not match.");
-        }
-    }
-
-    //Add data in user_account table
-    public void signup() throws SQLException {
         //Connect to database
         Database connectionNow = new Database();
         Connection connectDB = connectionNow.getConnection();
 
         //Create SQL code
-        String usernamefield = username.getText();
-        String namefield = name.getText();
-        String password = pass.getText();
-        String emailfield = email.getText();
-        RadioButton selectedRadioButton = (RadioButton) chucvu.getSelectedToggle();
-        String choose = selectedRadioButton.getText();
+        String choose = ((RadioButton) chucvu.getSelectedToggle()).getText();
         
-        PreparedStatement insertToRegister = connectDB.prepareStatement("INSERT INTO account VALUES (?,?,?,?,?)");
-        insertToRegister.setString(1, namefield);
-        insertToRegister.setString(2, emailfield);
-        insertToRegister.setString(3, usernamefield);
-        insertToRegister.setString(4, password);
-        insertToRegister.setString(5, choose);
+        PreparedStatement thêm_vào_database = connectDB.prepareStatement("INSERT INTO account VALUES (?,?,?,?,?)");
+        thêm_vào_database.setString(1, name.getText());
+        thêm_vào_database.setString(2, email.getText());
+        thêm_vào_database.setString(3, username.getText());
+        thêm_vào_database.setString(4, pass.getText());
+        thêm_vào_database.setString(5, choose);
     
-        //Verify if username already exist
-        String usernameDuplicate = "";
+        //username bị trùng
+        String trùng_username = "";
         try {
             Statement st = connectDB.createStatement();
-            ResultSet r1 = st.executeQuery("SELECT username FROM account WHERE username = '" + username + "'");
+            ResultSet r1 = st.executeQuery("SELECT username FROM account WHERE username = '" + username.getText() + "'");
 
             if(r1.next()) {
-                usernameDuplicate =  r1.getString("username");
+                trùng_username =  r1.getString("username");
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        //Verify if email already exist
-        String emailDuplicate = "";
+        //email bị trùng
+        String trùng_email = "";
         try {
             Statement st = connectDB.createStatement();
-            ResultSet r1 = st.executeQuery("SELECT email FROM account WHERE email = '" + email + "'");
+            ResultSet r1 = st.executeQuery("SELECT email FROM account WHERE email = '" + email.getText() + "'");
 
             if(r1.next()) {
-                emailDuplicate =  r1.getString("email");
+                trùng_email =  r1.getString("email");
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-        if (!namefield.equals("")) {
+         
+        if (!name.getText().equals("")) {
             namelabel.setText("");
-            if (!emailfield.equals(emailDuplicate)) {
-                emaillabel.setText("trùng");
-                if (!password.equals("")) {
-                    passlabel.setText("");
-                    if (!usernamefield.equals(usernameDuplicate)) {
-                        usernamelabel.setText("trung");
-                        if(!usernamefield.equals("")) {
-                            usernamelabel.setText("");
-                            try {
-                                insertToRegister.executeUpdate();
-                                signup.setDisable(true);
-                            } catch (SQLException e) {
-                                e.printStackTrace();
-                            }                                  
-                        } 
-                        else {
-                            usernamelabel.setText("Please insert your username.");    
+            if (!email.getText().equals("")) {
+                emaillabel.setText("");
+                if (!email.getText().equals(trùng_email)) {  
+                    if (!username.getText().equals("")) {
+                        usernamelabel.setText("");
+                        if (!username.getText().equals(trùng_username)) { 
+                            if (!pass.getText().equals("")) {
+                                passlabel.setText("");
+                                if (pass.getText().length() >= 8) {
+                                    if (!cfpass.getText().equals("")) {
+                                        cfpasslabel.setText("");
+                                        if (cfpass.getText().equals(pass.getText())) {
+                                            try {
+                                                thêm_vào_database.executeUpdate();
+                                            } catch (SQLException e) {
+                                                e.printStackTrace();
+                                            }  
+                                        } else {
+                                            cfpasslabel.setText("mật khẩu không khớp");
+                                        }
+                                    } else {
+                                        cfpasslabel.setText("nhập");
+                                    }
+                                } else {
+                                    passlabel.setText("mật khẩu quá ngắn");    
+                                }
+                            } else {
+                                passlabel.setText("nhập");
+                            }
+                        } else {
+                            usernamelabel.setText("username bị trùng");      
                         }
-                    } 
-                    else {
-                        usernamelabel.setText("Username already exist.");
+                    } else {
+                        usernamelabel.setText("nhập");
                     }
-                } 
-                else {
-                    passlabel.setText("Please insert a password.");
-                }           
+                } else {
+                    emaillabel.setText("email bị trùng");
+                }   
+            } else {
+                emaillabel.setText("nhập");
             }
-            else {
-                emaillabel.setText("E-mail already exist.");   
-            }
+        } else {
+            namelabel.setText("nhập");
         }
-        else {
-            namelabel.setText("Please insert your name.");
-        }   
-    } 
+    }
     
     @FXML
     public void loginOnAction(ActionEvent event) {
