c0e651987329a733ce753ee01eee5833b305fffb
myanhnguyen <114592790+myanhnguyen@users.noreply.github.com>
2022-11-27 00:26:14 +0700

    Update LoginController.java

--- BugManagement/src/app/LoginController.java
+++ BugManagement/src/app/LoginController.java
@@ -1,10 +1,7 @@
 package app;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -13,63 +10,51 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class LoginController implements Initializable {
-
+public class LoginController {
     @FXML
     private TextField username;
-    
     @FXML
     private PasswordField password;
- 
     @FXML
-    private Label loginlabel;
-    
-    
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-    }    
-    
+    private Label loginlabel;  
+      
+    @FXML
     public void loginOnAction(ActionEvent event) {
         if (!username.getText().isBlank() && !password.getText().isBlank()){
             Database connectNow = new Database();
             Connection connectDB = connectNow.getConnection();
 
-            String verifyLogin = "SELECT count(*) FROM account WHERE username = '" + username.getText() 
-                    + "' AND password = '" + password.getText() + "'";
-
             try {
                 Statement statement = connectDB.createStatement();
-                ResultSet queryResult = statement.executeQuery(verifyLogin);
+                ResultSet truy_vấn = statement.executeQuery("SELECT count(*) FROM account WHERE username = '" + username.getText() 
+                                                                + "' AND password = '" + password.getText() + "'");
 
-                while(queryResult.next()){
-                    if(queryResult.getInt(1) == 1) {
+                while(truy_vấn.next()){
+                    if(truy_vấn.getInt(1) == 1) {
                         try {
                             ChangeScene.Change(event,"App.fxml");
-                        }
-                        catch (Exception e1) {
+                        } catch (Exception e1) {
                             e1.printStackTrace();
                         }
-                    }
-                    else {
+                    } else {
                         loginlabel.setText("Hãy nhập lại");
-
                     }
                 }
-            }
-            catch (SQLException e2) {
+            } catch (SQLException e2) {
                 e2.printStackTrace();
             }
         }
-    }
+    }              
+                              
+
+            
      
-   public void signupOnAction(ActionEvent event) {
+    @FXML
+    public void signupOnAction(ActionEvent event) {
         try {
             ChangeScene.Change(event,"Signup.fxml");
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-   }
+    }
 }
-
