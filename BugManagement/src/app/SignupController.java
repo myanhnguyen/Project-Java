53e158257d689696aaaae27a7be5f1b6720ea280
myanhnguyen <114592790+myanhnguyen@users.noreply.github.com>
2022-11-27 10:52:07 +0700

    Update SignupController.java

--- BugManagement/src/app/SignupController.java
+++ BugManagement/src/app/SignupController.java
@@ -31,20 +31,24 @@ public class SignupController {
     @FXML
     private Label cfpasslabel;
     @FXML
-    private ToggleGroup chucvu;
-    @FXML
     private Label passlabel;
     @FXML
     private Label namelabel;  
+    @FXML
+    private Label chooselabel;
+    @FXML
+    private Label donelabel;
+    @FXML
+    private ToggleGroup chức_vụ;
 
     @FXML
-    public void singupOnAction() throws SQLException {
+    public void singupOnAction(ActionEvent event) throws SQLException {
         //Connect to database
         Database connectionNow = new Database();
         Connection connectDB = connectionNow.getConnection();
 
         //Create SQL code
-        String choose = ((RadioButton) chucvu.getSelectedToggle()).getText();
+        String choose = ((RadioButton) chức_vụ.getSelectedToggle()).getText();
         
         PreparedStatement thêm_vào_database = connectDB.prepareStatement("INSERT INTO account VALUES (?,?,?,?,?)");
         thêm_vào_database.setString(1, name.getText());
@@ -93,11 +97,18 @@ public class SignupController {
                                     if (!cfpass.getText().equals("")) {
                                         cfpasslabel.setText("");
                                         if (cfpass.getText().equals(pass.getText())) {
-                                            try {
-                                                thêm_vào_database.executeUpdate();
-                                            } catch (SQLException e) {
-                                                e.printStackTrace();
-                                            }  
+                                            if (!choose.equals("")) {
+                                                chooselabel.setText("");
+                                                try {
+                                                    thêm_vào_database.executeUpdate();
+                                                    ChangeScene.Change(event,"Login.fxml");
+                                                    donelabel.setText("Đăng ký thành công!");
+                                                } catch (SQLException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            } else {
+                                                chooselabel.setText("chọn");
+                                            }
                                         } else {
                                             cfpasslabel.setText("mật khẩu không khớp");
                                         }
@@ -125,6 +136,55 @@ public class SignupController {
         } else {
             namelabel.setText("nhập");
         }
+        
+        //check 
+        if (!name.getText().equals("")) {
+            namelabel.setText("");
+        } else {
+            namelabel.setText("nhập");
+        }
+        
+        if (!email.getText().equals("")) {
+            emaillabel.setText("");
+            if (email.getText().equals(trùng_email)) {
+                emaillabel.setText("email bị trùng");
+            }   
+        } else {
+            emaillabel.setText("nhập");
+        }
+        
+        if (!username.getText().equals("")) {
+            usernamelabel.setText("");
+            if (username.getText().equals(trùng_username)) { 
+                usernamelabel.setText("username bị trùng");
+            }
+        } else {
+            usernamelabel.setText("nhập");
+        }    
+        
+        if (!pass.getText().equals("")) {
+            passlabel.setText("");
+            if (pass.getText().length() < 8) {
+                passlabel.setText("mật khẩu quá ngắn");  
+            }
+        } else {
+            passlabel.setText("nhập");
+        }
+        
+        if (!cfpass.getText().equals("")) {
+            cfpasslabel.setText("");
+            if (!cfpass.getText().equals(pass.getText())) {
+                cfpasslabel.setText("mật khẩu không khớp");
+            } 
+        } else {
+            cfpasslabel.setText("nhập");
+        }
+        
+        if (!choose.equals("")) {
+            chooselabel.setText("");
+        } else {
+            chooselabel.setText("chọn");
+        }          
     }
     
     @FXML
